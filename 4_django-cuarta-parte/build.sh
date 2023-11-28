832f9c6e25c929ba3dde80c7f502da8485789707
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-28 15:39:00 -0300

    Update build.sh

--- 4_django-cuarta-parte/build.sh
+++ 4_django-cuarta-parte/build.sh
@@ -4,7 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-mkdir staticfiles media
+mkdir static staticfiles media
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
