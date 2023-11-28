736ff42b203a7aefab21a1e76ddfdb2ddcba391e
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-28 16:15:45 -0300

    Update wsgi.py

--- 4_django-cuarta-parte/products_api/wsgi.py
+++ 4_django-cuarta-parte/products_api/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'products_api.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
