3eec3a0d7150eaed55b47e9df68052f0f24a3e6d
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-28 15:15:10 -0300

    Update settings.py

--- 4_django-cuarta-parte/products_api/settings.py
+++ 4_django-cuarta-parte/products_api/settings.py
@@ -134,6 +134,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = "static"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
