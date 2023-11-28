2c1744b235d5dd0baa6c7e65fcfa4741666f7f37
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-28 16:06:48 -0300

    Update settings.py

--- 4_django-cuarta-parte/products_api/settings.py
+++ 4_django-cuarta-parte/products_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--&x()6cgh82@y#q$ww^tro6-vayp4^bor6#jy=$dxgvuk=w09+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
