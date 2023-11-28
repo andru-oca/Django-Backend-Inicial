b26a2a6775e204cac965747740bd2bde16f0afeb
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-28 16:11:19 -0300

    Update settings.py

--- 4_django-cuarta-parte/products_api/settings.py
+++ 4_django-cuarta-parte/products_api/settings.py
@@ -50,6 +50,7 @@ CUSTOM_MIDDLEWARE = ["app_products.middleware_cors.CorsMiddlewareMixin"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
